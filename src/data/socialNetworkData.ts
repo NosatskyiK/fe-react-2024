888d66d6d6d46e74f1eaa2f3ef3d043a1942b815
Kostya Nosatskyi <kostyanosatskyi@MacBook-Pro-Kostya.local>
2024-04-27 21:45:22 +0300

    fixed bugs

--- src/data/socialNetworkData.ts
+++ src/data/socialNetworkData.ts
@@ -2,17 +2,17 @@ import type SocialNetwork from '@/interface/interfaceSocialNetwork.ts';
 
 const SocialNetworkData: SocialNetwork[] = [
     {
-        img: '/fe-react-2024/src/assets/img/footerImg/facebook.png',
+        img: './src/assets/img/footerImg/facebook.png',
         url: 'https://www.facebook.com/profile.php?id=100015349961940',
         altName: 'Facebook',
     },
     {
-        img: '/fe-react-2024/src/assets/img/footerImg/instagram.png',
+        img: './src/assets/img/footerImg/instagram.png',
         url: 'https://www.instagram.com/nosatskyi.k/',
         altName: 'Instagram',
     },
     {
-        img: '/fe-react-2024/src/assets/img/footerImg/linkedIn.png',
+        img: './src/assets/img/footerImg/linkedIn.png',
         url: 'https://www.linkedin.com/in/костянтин-носацький-4617722a5/',
         altName: 'LinkedIn',
     },
