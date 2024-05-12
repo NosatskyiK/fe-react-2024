a04c9201e7025b8c464f8a9ac1e9ba90403f7d43
Kostya Nosatskyi <kostyanosatskyi@MacBook-Pro-Kostya.local>
2024-05-12 18:59:09 +0300

    removed the unnecessary fragment

--- src/components/productCard/ProductCard.component.tsx
+++ src/components/productCard/ProductCard.component.tsx
@@ -6,14 +6,12 @@ import type { Product } from '@/interface/interfaceProductCategory.ts';
 import styles from './productCard.module.css';
 
 export const ProductCard: React.FC<{ product: Product }> = ({ product }) => (
-    <>
-        <div className={styles.card}>
-            <img className={styles.cardImage} src={product.images} alt="Product" />
-            <h2 className={styles.cardTitle}>{product.title}</h2>
-            <div className={styles.cardBuy}>
-                <span className={styles.cardPrice}>{product.price} </span>
-                <Cart />
-            </div>
+    <div className={styles.card}>
+        <img className={styles.cardImage} src={product.images} alt="Product" />
+        <h2 className={styles.cardTitle}>{product.title}</h2>
+        <div className={styles.cardBuy}>
+            <span className={styles.cardPrice}>{product.price} </span>
+            <Cart />
         </div>
-    </>
+    </div>
 );
